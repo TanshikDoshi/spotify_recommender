fbfcbe8553138a330794b57c2ed6b3ae4eb2079b
TanshikDoshi <121330852+TanshikDoshi@users.noreply.github.com>
2023-03-29 08:58:36 +0530

    Update spotify_recommender.py

--- spotify_recommender.py
+++ spotify_recommender.py
@@ -2,6 +2,9 @@ import pandas as pd
 import streamlit as st
 df = pd.read_csv('final.csv')
 
+st.write("""
+# 890 Recommender
+""")
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
 
@@ -13,19 +16,19 @@ df[features] = scaler.fit_transform(df[features])
 cosine_sim = cosine_similarity(df[features], df[features])
 #print(cosine_sim)
 
-def get_recommendations(title, number_of_recommendations=5):
+def get_recommendations(title):
     song_index = df[df['Track Name'] == title].index[0]
     sim_scores = list(enumerate(cosine_sim[song_index]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    top_indices = [i[0] for i in sim_scores[1:number_of_recommendations+1]]
+    top_indices = [i[0] for i in sim_scores[1:5+1]]
     r = df['Track Name'].iloc[top_indices]
     return r.to_frame(name="Song")
 
 text_input = st.text_input(
-        "Enter anything"
+        "Enter a Song"
     )
 
 if text_input:
-    best_index = get_recommendations(text_input)
+    recommendations = get_recommendations(text_input)
     st.subheader('Recommendation')
-    st.write(df[['Song']].iloc[best_index])
+    st.write(recommendnations)
